57e2da4250aeb2ffe361d64d181c24790a892ed9
Kyle Tilton <kyletilton23@gmail.com>
2016-08-22 14:50:26 -0700

    printf function copied from book

--- bio.c
+++ bio.c
@@ -3,4 +3,28 @@ int prints(char *s) { while(*s) putc(*s++); }
 
 int gets() {}
 
-int printf() {}
+int printf(char *fmt, ...) 
+{
+  char * = fmt;
+  u16 *ip = (u16 *)&fmt + 1;
+  u32 *up;
+  while (*cp) {
+    if (*cp != '%') {
+      putc(*cp);
+      if(*cp=='\n')
+        putc('\r');
+      cp++; continue;
+    }
+    cp++;
+    switch(*cp) {
+      case 'c' :   putc(*ip); break;
+      case 's' : prints(*ip); break;
+      case 'u' : printu(*ip); break;
+      case 'd' : printd(*ip); break;
+      case 'x' : printx(*ip); break;
+      case 'l' : printl(*ip); break;
+      case 'X' : printX(*ip); break;
+    }
+    cp++; ip++;
+  }
+}
